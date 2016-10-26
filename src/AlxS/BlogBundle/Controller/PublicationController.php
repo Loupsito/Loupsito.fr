848c30c40a2195565613a09a2b4bb9ad288c59ac
Alexandre SUY <alexandre.suy@gmail.com>
2016-10-26 23:59:12 +0200

    Adding signUp page + change CSS

--- src/AlxS/BlogBundle/Controller/PublicationController.php
+++ src/AlxS/BlogBundle/Controller/PublicationController.php
@@ -5,6 +5,8 @@ namespace AlxS\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use AlxS\BlogBundle\Entity\User;
+
 
 class PublicationController extends Controller {
 
@@ -28,7 +30,7 @@ class PublicationController extends Controller {
         /*
          * Ici se fera une recherche dans une entité, pour récupéré les données sur l'utilisateur courant
          */
-        
+
         //Array to send
         $Arr = array(
             'urlPicture' => 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAXKAAAAJDhmOGIxNTQyLWUxYzMtNDU1OS04NDUzLTkzNGIwNzU1MWFlYw.jpg',
@@ -37,7 +39,7 @@ class PublicationController extends Controller {
 		My differents experiences have led me to tell
                 me I had to make this world, my expertise and
                 my future profession.',
-            'userName' => $userName,            
+            'userName' => $userName,
         );
 
         //Recover of the session
@@ -46,13 +48,42 @@ class PublicationController extends Controller {
 
         //If the session exist
         if ($session->has('nameUser')) {
-           $Arr['connecte'] = "true";
+            $Arr['connecte'] = "true";
         }
         else {
-           $Arr['connecte'] = "false"; 
+            $Arr['connecte'] = "false";
         }
 
         return $this->render('AlxSBlogBundle:Publication:pageUser.html.twig', $Arr);
+    }
+
+    public function signUpAction(Request $request) {
+
+        // On crée un objet Advert
+        $advert = new User();
+
+        // On crée le FormBuilder grâce au service form factory
+        $formBuilder = $this->get('form.factory')->createBuilder('form', $advert);
+
+        // On ajoute les champs de l'entité que l'on veut à notre formulaire
+        $formBuilder
+                ->add('LastName', 'text')
+                ->add('FirstName', 'text')
+                ->add('BirthDate', 'date')
+                ->add('mail', 'text')
+                ->add('login', 'text')
+                ->add('passwd', 'text')
+        ;
+        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
+        // À partir du formBuilder, on génère le formulaire
+        $form = $formBuilder->getForm();
+
+        // On passe la méthode createView() du formulaire à la vue
+        // afin qu'elle puisse afficher le formulaire toute seule
+        return $this->render('AlxSBlogBundle:Publication:signUp.html.twig', array(
+                    'connecte'=>'false',
+                    'form' => $form->createView(),
+        ));
     }
 
 }
